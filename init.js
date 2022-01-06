8caf393cf33fe3b126b92fe09a0c818d3a817131
Rishi Raj <rishiraj.1012exp@gmail.com>
2022-01-07 00:49:12 +0530

    GHA Test 4

--- init.js
+++ init.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-console.log('Running init script...');
+console.log('Running init script..');
 
 // Connect to MongoDB server
 const prompt = require('prompt');
