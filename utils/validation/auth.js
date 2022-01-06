95734c4d155b887d20240359050422edc1bfeb2d
Bimalesh Seth <sethbimalesh@users.noreply.github.com>
2022-01-06 18:15:02 +0530

    forgot password added

--- utils/validation/auth.js
+++ utils/validation/auth.js
@@ -1,6 +1,17 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
-module.exports.checkLogin = Joi.object({
-  email: Joi.string().trim().email().required().label("Email"),
-  password: Joi.string().trim().min(8).required().label("Password"),
+const checkLogin = Joi.object({
+  email: Joi.string().trim().email().required().label('Email'),
+  password: Joi.string().trim().min(8).required().label('Password'),
 });
+
+const checkResetPassword = Joi.object({
+  token: Joi.string().trim().length(64).required().label('Token'),
+  password: Joi.string().trim().min(8).required().label('Password'),
+  password2: Joi.any()
+    .valid(Joi.ref('password'))
+    .required()
+    .label('Confirm Password'),
+});
+
+module.exports = { checkLogin, checkResetPassword };
